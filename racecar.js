492b3b391116279e3ab5bf47c05b382ee85f3a8c
riri <nadalariamae@gmail.com>
2026-03-14 14:19:50 +0800

    Update racecar.js

--- racecar.js
+++ racecar.js
@@ -1,6 +1,6 @@
 const score = document.querySelector('.score');
 const highScore = document.querySelector('.highScore');
-const startScreen = document.querySelector('.startScreen')
+const startScreen = document.querySelector('.startScreen');
 const gameArea = document.querySelector('.gameArea');
 const ClickToStart = document.querySelector('.ClickToStart');
 
